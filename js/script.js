9d7da63039f8dfc857634a94676f180128915609
default <lucas.nguyen@mths.ca>
2025-03-17 17:26:23 +0000

    code all good

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Lucas Nguyen All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lucas Nguyen
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
 
-/**
- * Displays an alert with the message "Hello, World!"
- */
-function myButtonClicked() {
-  alert("Hello, World!")
+function enterClicked() {
+  // input
+  const streetNumeber = document.getElementById("street-number").value
+  const streetName = document.getElementById("street-name").value
+  // output
+    document.getElementById("address").innerHTML =
+    "Your address is: " + streetNumeber + ", " + streetName + "."
 }
